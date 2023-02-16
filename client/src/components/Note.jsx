2e4c26478d6d6dbf3b348e48d7287866d92debf9
dinhhung1598753 <hoangdinhhung159@gmail.com>
2023-02-17 02:38:42 +0700

    add draft to hmtl

--- client/src/components/Note.jsx
+++ client/src/components/Note.jsx
@@ -9,11 +9,23 @@ import {
   EditorState,
 } from "draft-js";
 
+import draftToHtml from "draftjs-to-html";
+
 export default function Note({ note }) {
   const [editorState, setEditorState] = useState(() => {
     return EditorState.createEmpty();
   });
+  useEffect(() => {
+    const blocksFromHTML = convertFromHTML(note.content);
+    const state = ContentState.createFromBlockArray(
+      blocksFromHTML.contentBlocks,
+      blocksFromHTML.entityMap
+    );
+    setEditorState(EditorState.createWithContent(state));
+  }, [note.id]);
+  
   const onEditorStateChange = (state) => {
+    console.log(draftToHtml(convertToRaw(state.getCurrentContent())));
     setEditorState(state);
   };
   return (
